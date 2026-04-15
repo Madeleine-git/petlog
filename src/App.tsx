3a0121ce7a057b0d964c978624dd60b6470163f6
Madeleine Urrego <madeleine.urrego@alu.ceacfp.es>
2026-04-15 12:55:49 +0200

    feat: estructura inicial del proyecto

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-green-100">
+      <h1 className="text-3xl font-bold text-green-800">PetLog funciona</h1>
+    </div>
+  )
+}
+
+export default App
